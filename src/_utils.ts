4fddbd6fa60173756296b2c702f3bfea7f3e0512
Dominic Birch <dominicbirch@outlook.com>
2020-08-26 17:46:22 +0100

    Corrected return types on factories

--- src/_utils.ts
+++ src/_utils.ts
@@ -81,49 +81,49 @@ function hasValue<T>(v: T): boolean {
     return true;
 }
 
-export function requireValue(message: string = "Please provide an answer") {
+export function requireValue(message: string = "Please provide an answer"): ValidationHandler {
     return <T>(v: T) => hasValue(v)
         ? null
         : [message];
 }
 
-export function requireMinimum(min: number, message?: string): Validator<number> {
+export function requireMinimum(min: number, message?: string): ValidationHandler<number> {
     return v => (hasValue(v) && v < min)
         ? [message || `The value must not be less than ${min}`]
         : null;
 }
 
-export function requireMaximum(max: number, message?: string): Validator<number> {
+export function requireMaximum(max: number, message?: string): ValidationHandler<number> {
     return v => (hasValue(v) && v > max)
         ? [message || `The value must not be more than ${max}`]
         : null;
 }
 
-export function requireBetween(min: number, max: number, message?: string): Validator<number> {
+export function requireBetween(min: number, max: number, message?: string): ValidationHandler<number> {
     return v => (hasValue(v) && (v < min || v > max))
         ? [message || `The value should be between ${min} and ${max}`]
         : null;
 }
 
-export function requireMinimumLength(min: number, message?: string): Validator<string | any[]> {
+export function requireMinimumLength(min: number, message?: string): ValidationHandler<string | any[]> {
     return v => (hasValue(v) && v.length < min)
         ? [message || (Array.isArray(v) ? `There should be at least ${min} items` : `The value should be at least ${min} characters in length`)]
         : null;
 }
 
-export function requireMaximumLength(max: number, message?: string): Validator<string | any[]> {
+export function requireMaximumLength(max: number, message?: string): ValidationHandler<string | any[]> {
     return v => (hasValue(v) && v.length > max)
         ? [message || (Array.isArray(v) ? `There should be no more than ${max} items` : `The value should be no more than ${max} characters in length`)]
         : null;
 }
 
-export function requireLengthBetween(min: number, max: number, message?: string): Validator<string | any[]> {
+export function requireLengthBetween(min: number, max: number, message?: string): ValidationHandler<string | any[]> {
     return v => (hasValue(v) && (v.length < min || v.length > max))
         ? [message || (Array.isArray(v) ? `There should be between ${min} and ${max} items` : `The value should be between ${min} and ${max} characters in length`)]
         : null;
 }
 
-export function requirePattern(pattern: RegExp, message = "The answer provided is invalid"): Validator<string> {
+export function requirePattern(pattern: RegExp, message = "The answer provided is invalid"): ValidationHandler<string> {
     return v => pattern.test(v || "")
         ? null
         : [message];
